6cc4e64c1d69e6cee6229bd46d40224a1082d2fb
OwenLu <gg0125gg@gmail.com>
2023-05-27 22:27:01 +0800

    feat: add state to store todo input

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,5 +1,5 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
-
+import { useState } from 'react';
 const dummyTodos = [
   {
     title: 'Learn react-router',
@@ -24,11 +24,19 @@ const dummyTodos = [
 ];
 
 const TodoPage = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInput = (value) => {
+    setInputValue(value)
+  };
   return (
     <div>
       TodoPage
       <Header />
-      <TodoInput />
+      <TodoInput 
+      inputValue={inputValue}
+      onChange={handleInput}
+      />
       <TodoCollection todos={dummyTodos}/>
       <Footer />
     </div>
