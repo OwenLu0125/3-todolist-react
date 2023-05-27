5652657b9015fc4c1ac52ad0cbddc0665350783f
OwenLu <gg0125gg@gmail.com>
2023-05-27 22:48:57 +0800

    feat: add save button event handler

--- src/components/TodoCollection.jsx
+++ src/components/TodoCollection.jsx
@@ -1,7 +1,7 @@
 import TodoItem from './TodoItem';
 
 const TodoCollection = ({
-  todos, 
+  todos,
   onSave,
   onDelete,
   onToggleDone,
@@ -10,7 +10,7 @@ const TodoCollection = ({
   return (
     <div>
       {todos.map((todo) => {
-        return <TodoItem key={todo.id} todo={ todo }/>;
+        return <TodoItem key={todo.id} todo={todo} />;
       })}
     </div>
   );
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -25,16 +25,36 @@ const dummyTodos = [
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
-  const [todos, setRodos] = useState(dummyTodos)
+  const [todos, setTodos] = useState(dummyTodos);
 
   const handleInput = (value) => {
-    setInputValue(value)
+    setInputValue(value);
+  };
+
+  const handleTodo = () => {
+    if (inputValue.length === 0) {
+      return;
+    }
+    setTodos((prevTodos) => {
+      return [
+        ...prevTodos,
+        {
+          id: Math.random() * 100,
+          title: inputValue,
+          isDone: false,
+        },
+      ];
+    });
   };
   return (
     <div>
       TodoPage
       <Header />
-      <TodoInput inputValue={inputValue} onChange={handleInput} />
+      <TodoInput
+        inputValue={inputValue}
+        onChange={handleInput}
+        onAddTodo={handleTodo}
+      />
       <TodoCollection todos={todos} />
       <Footer />
     </div>
